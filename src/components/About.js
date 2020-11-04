894252f17355296c6ae6169b395ee483cb9c2c8e
Rachel Kline <rach3lkline@gmail.com>
2020-11-04 16:40:52 -0500

    About Me Section

---
+++ src/components/About.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+export default class About extends Component {
+  render() {
+    let resumeData = this.props.resumeData;
+    return (
+      <section id="about">
+         <div className="row">
+
+            <div className="three columns">
+
+               <img className="profile-pic"  src="images/profilepic.jpg" alt="" />
+
+            </div>
+
+            <div className="eight columns main-col">
+
+               <h2 class="abtMe">About Me</h2>
+               <p class="abtMe">
+               {
+                 resumeData.aboutme
+               }
+               </p>
+
+               <div className="row">
+
+                  <div className="columns contact-details">
+
+                  <h2>Contact Details</h2>
+                  <p className="address">
+       						<span>{resumeData.name}</span>
+                     <br></br>
+       						   <span>
+                     {resumeData.address}
+                    </span>
+                    <br></br>
+                    <a>{resumeData.website}</a>
+       					   </p>
+                  </div>
+               </div>
+            </div>
+         </div>
+      </section>
+    );
+  }
+}
--- src/resumeData.js
+++ src/resumeData.js
@@ -1,7 +1,54 @@
 let resumeData = {
     "name": "Rachel Kline",
     "role": "Full Stack Developer",
-    "roleDescription": "specializing in HTML, Javascript, CSS, MERN Stack, React, Node, Handlebars, mySQL, MongoDB, Mongoose and more!"
+    "roleDescription": "specializing in HTML, Javascript, CSS, MERN Stack, React, Node, Handlebars, mySQL, MongoDB, Mongoose and more!",
+    "aboutme": "I am a Full Stack Web Developer student currently enrolled in University of Pennsylvania's Coding Bootcamp program. I'm set to graduate in November and I currently have an A+ average. I'm also the lead singer of 'Philadelphia Funk Authority,' an award-winning party band that performs on stages all over the tri-state area.",
+    "address": "Philadelphia, PA",
+    "website": "https://rachelkline.github.io/",
+    "education": [
+      {
+        "UniversityName": "University of Pennsylvania",
+        "specialization": "Full Stack Development",
+        "MonthOfPassing": "November",
+        "YearOfPassing": "2020",
+        "Achievements": "Certificate"
+      },
+      {
+        "UniversityName": "Saint Joseph's University",
+        "specialization": "Communication Studies",
+        "MonthOfPassing": "May",
+        "YearOfPassing": "2017",
+        "Achievements": "Bachelors"
+      }
+    ],
+    "work": [
+      {
+        "CompanyName": "Dürr Systems",
+        "specialization": "Logistics Manager",
+        "MonthOfLeaving": "July",
+        "YearOfLeaving": "2018-November 2019",
+        "Achievements": "Worked to make sure that the parts and equipment are delivered to the installation team on time and in the condition required for proper installation"
+      },
+      {
+        "CompanyName": "Nissan Automotive of North America",
+        "specialization": "Purchasing Analyst",
+        "MonthOfLeaving": "May",
+        "YearOfLeaving": "2017-June 2018",
+        "Achievements": "Analyze cost reduction data and related processes while making recommendations for improvement when possible"
+      }
+    ],
+    "skillsDescription": "",
+    "skills": [
+      {
+        "skillname": "HTML5"
+      },
+      {
+        "skillname": "CSS"
+      },
+      {
+        "skillname": "Reactjs"
+      }
+    ],
 }
 
 export default resumeData
