169df2430e7aa3aa26a3edf637e94e4b781f25d8
Rachel Kline <rach3lkline@gmail.com>
2020-11-04 16:45:31 -0500

    Added Profile Pic

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Header from "./components/Header";
+import About from './components/About';
 import resumeData from './resumeData';
 
 function App() {
   return (
+    <>
     <Header resumeData={resumeData}/>
+    <About resumeData={resumeData} />
+    </>
   );
 }
 
