dc06a387448eeb1e864f870ec379a2819af8630d
Rachel Kline <rach3lkline@gmail.com>
2020-11-04 22:23:24 -0500

    Heroku Deployment

--- public/init.js
+++ public/init.js
@@ -11,6 +11,23 @@ $(window).on('resize', function() {
     $('body').css({ 'width': $(window).width() })
 });
 
+/*----------------------------------------------------*/
+/* Smooth Scrolling
+------------------------------------------------------ */
+
+$('.smoothscroll').on('click',function (e) {
+    e.preventDefault();
+
+    var target = this.hash,
+    $target = $(target);
+
+    $('html, body').stop().animate({
+        'scrollTop': $target.offset().top
+    }, 800, 'swing', function () {
+        window.location.hash = target;
+    });
+});
+
 
 /*----------------------------------------------------*/
 /*	Fade In/Out Primary Navigation
--- src/components/Header.js
+++ src/components/Header.js
@@ -23,11 +23,23 @@ export default class Header extends Component {
                  <h1 className="responsive-headline">I'm {resumeData.name}.</h1>
                  <h3 style={{color:'#fff', fontFamily:'sans-serif '}}>I am a {resumeData.role} {resumeData.roleDescription}
                  </h3>
+                 <ul className="social">
+                  {
+                    resumeData.socialLinks && resumeData.socialLinks.map(item =>{
+                      return(
+                              <li key={item.name}>
+                                <a href={item.url} target="_blank"><i className={item.className}></i></a>
+                              </li>
+                            )
+                          }
+                    )
+                  }
+               </ul>
               </div>
            </div>
   
            <p className="scrolldown">
-              <a className="smoothscroll" href="#about"><i className="icon-down-circle"></i></a>
+              <a className="smoothscroll" href="#about"></a>
            </p>
   
         </header>
