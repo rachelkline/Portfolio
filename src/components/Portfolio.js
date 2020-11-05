57aedf8f369c284a6f6da730fc4e330b656870aa
Rachel Kline <rach3lkline@gmail.com>
2020-11-04 19:39:43 -0500

    Portfolio page plus images

---
+++ src/components/Portfolio.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+// import react-router from 'react-router';
+import {BrowserRouter as Router, Switch, Route, Link} from 'react-router-dom';
+export default class Porfolio extends Component {
+  render() {
+    let resumeData = this.props.resumeData;
+    return (
+      <Router>
+
+  </Router>
+        );
+  }
+}
